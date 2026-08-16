a03f8a1cf696418d0361a802cf46ff6e401f479f
Roaa-Hussam <roaahussam911@gmail.com>
2026-08-17 02:50:55 +0300

    Improve responsive hero section

--- src/components/HeroHome.jsx
+++ src/components/HeroHome.jsx
@@ -30,7 +30,7 @@ const HeroHome = () => {
         <br />
         and study easily from any part of the world.
       </p>
-      <p className="mx-24 mt-8">
+      <p className="mx-24 mt-16">
         <Link
           to="/login"
           className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
